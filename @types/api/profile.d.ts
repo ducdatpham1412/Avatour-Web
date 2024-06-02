9365930d86b864d2481076ac1352cee97f65eb6e
jay <phamducdat14122000@gmail.com>
2024-06-02 23:48:39 +0700

    refactor: interface profile

--- @types/api/profile.d.ts
+++ @types/api/profile.d.ts
@@ -2,49 +2,55 @@ type Service =
   /**
    * 1. Backpack
    */
-  | 'backpack'
+  | 'backpack' // Đi phượt
   // Traditional
-  | 'culture'
-  | 'creative'
-  | 'history'
-  | 'pagoda'
-  | 'catholic'
+  | 'culture' // Văn hoá
+  | 'creative' // Sáng tạo
+  | 'art' // Nghệ thuật
+  | 'history' // Lịch sử
+  | 'museum' // Bảo tàng
+  | 'pagoda' // Đền, Chùa
+  | 'catholic' // Nhà thờ
   // Chill
-  | 'check-in'
-  | 'flower'
-  | 'beach'
+  | 'check-in' // Check-in
+  | 'flower' // Địa điểm hoa
+  | 'beach' // Bãi biển
+  | 'book' //  Sách
+  | 'park' // Công viên
   /**
    * 2. Cuisine
    */
-  | 'cuisine'
-  | 'food'
-  | 'breakfast'
-  | 'lunch'
-  | 'dinner'
-  | 'other-food'
-  | 'drink'
+  | 'cuisine' // Ẩm thực
+  | 'food' // Nhà hàng/Quán ăn
+  | 'breakfast' // Ăn sáng
+  | 'lunch' // Ăn trưa
+  | 'dinner' // Ăn tối
+  | 'drink' // Đồ uống
+  | 'pub' // Bar, Pub
+  | 'other-food' // Ẩm thực khác
   /**
    * 3. Other categories
    */
-  | 'volunteer'
-  | 'camping'
-  | 'team-building'
+  | 'volunteer' // Tình nguyện
+  | 'camping' // Cắm trại
+  | 'team-building' // Team-building
   /**
    * 4. Travel services
    */
-  | 'travel-service'
-  | 'transport-to'
-  | 'transport-in'
-  | 'bicycle'
-  | 'homestay'
-  | 'hotel'
-  | 'shopping'
-  | 'fishing'
-  | 'other-service';
+  | 'travel-service' // Dịch vụ du lịch
+  | 'transport-to' // Xe đi
+  | 'transport-in' // Thuê xe
+  | 'bicycle' // Thuê xe đạp
+  | 'homestay' // Homestay
+  | 'hotel' // Khách sạn
+  | 'shopping' // Mua sắm
+  | 'fishing' // Câu cá
+  | 'entertainment' // Giải trí
+  | 'other-service'; // Dịch vụ khác
 
 interface TypeProfile {
   id: number;
-  account_type: number;
+  account_type: 'holder' | 'admin' | 'user' | 'shop' | 'location' | 'tour-guide';
   email: string | null;
   phone: string | null;
   name: string;
@@ -64,8 +70,7 @@ interface TypeProfile {
   end_time: number;
   total_ratings: number;
   average_stars: number;
-  services: Array<number>;
-  link: string;
+  link: string[];
   rank: number;
   status: number;
 }
