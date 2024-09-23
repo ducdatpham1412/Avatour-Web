c2c82cef0f8730e2033d3a0b333573cef5162442
jay <phamducdat14122000@gmail.com>
2024-09-23 14:25:53 +0700

    feat/table_of_content

--- src/features/magazine/DetailMagazine.tsx
+++ src/features/magazine/DetailMagazine.tsx
@@ -5,17 +5,57 @@ import { Fragment } from 'react';
 
 import Container from '@/app/container';
 import { useAppContext } from '@/app/provider';
-import { Avatar, Image } from '@/components/ui';
 import { DropDown } from '@/components';
-import { getMarginContentMagazine, goToProfile, logger, parseErrorMessage } from '@/lib';
-import { setMagazine } from '@/lib/storage';
-import { MAGAZINE_ROUTES } from '@/configs/routes';
 import { DialogConfirm } from '@/components/dialogs';
+import { Avatar, Image } from '@/components/ui';
+import { MAGAZINE_ROUTES } from '@/configs/routes';
 import { toast } from '@/hooks';
+import { cn, getMarginContentMagazine, goToProfile, logger, parseErrorMessage } from '@/lib';
+import { setMagazine } from '@/lib/storage';
 
 import { useMagazines } from './hooks';
 
 type Props = PageProps<{ magazine_id: string }>;
+
+type TableOfContentProps = {
+  listTitles: MagazineContent[];
+  className: string;
+};
+
+const getElementTitleId = (title: string) => {
+  return `title-${title}`;
+};
+
+const TableOfContent = ({ listTitles, className }: TableOfContentProps) => {
+  return (
+    <div
+      className={cn(
+        'rounded-[14px] border-[1px] border-gray_300 bg-white px-[12px] py-[8px] flex-col gap-y-2',
+        className,
+      )}
+    >
+      <h1 className="text-[18px] font-medium">Mục lục</h1>
+      <div className="w-full h-[1px] bg-gray_200" />
+      {listTitles.map(t => {
+        return (
+          <button
+            className="hover:underline text-start text-[14px] lg:text-[12px]"
+            onClick={() => {
+              const element = document.getElementById(getElementTitleId(t.content));
+              element?.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center',
+                inline: 'center',
+              });
+            }}
+          >
+            {t.content}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
 
 const DetailMagazine = ({ params }: Props) => {
   const router = useRouter();
@@ -27,6 +67,8 @@ const DetailMagazine = ({ params }: Props) => {
   if (!magazine) {
     return <Container />;
   }
+
+  const listTitles = magazine.content.filter(c => c.type === 'title');
 
   const renderContent = (content: MagazineContent) => {
     const { top, bottom } = getMarginContentMagazine(content);
@@ -51,94 +93,112 @@ const DetailMagazine = ({ params }: Props) => {
     }
 
     return (
-      <p className="text-[20px] font-medium" style={{ marginTop: top, marginBottom: bottom }}>
+      <section
+        id={getElementTitleId(content.content)}
+        className="text-[20px] font-medium"
+        style={{ marginTop: top, marginBottom: bottom }}
+      >
         {content.content}
-      </p>
+      </section>
     );
   };
 
   return (
     <Container
       showHeader={false}
-      contentContainer="max-w-[750px] px-[20px]"
+      contentContainer="max-w-[750px] lg:max-w-[1040px] px-[20px]"
       metaData={{
         title: magazine.title,
         descriptions: magazine.description,
         keywords: magazine.keywords,
       }}
     >
-      <p className="text-[28px] md:text-[36px] font-medium mt-[12px]">{magazine.title}</p>
+      <div className="w-full inline-flex gap-[40px]">
+        <div className="flex flex-1 flex-col">
+          <p className="text-[28px] md:text-[36px] font-medium mt-[12px]">{magazine.title}</p>
 
-      <div className="w-full inline-flex justify-between mt-[24px] mb-[4px]">
-        <button
-          className="inline-flex items-center gap-2 hover-scale"
-          onClick={() =>
-            goToProfile(magazine.creator, {
-              router,
-              myId: profile?.id,
-            })
-          }
-        >
-          <Avatar src={magazine.creator_avatar} size={35} />
-          <p>{magazine.creator_name}</p>
-        </button>
-        {isMine && (
-          <DropDown
-            trigger={
-              <EllipsisVertical strokeWidth={1.4} size={20} className="hover-slow rounded-md" />
-            }
-            options={[
-              {
-                value: 'edit',
-                label: 'Chỉnh sửa',
-                type: 'menu-item',
-              },
-              {
-                value: 'delete',
-                label: 'Xoá bài',
-                type: 'menu-item',
-              },
-            ]}
-            onCheck={v => {
-              if (v === 'delete') {
-                setTimeout(() => {
-                  DialogConfirm.open({
-                    title: 'Bạn chắc chắn muốn xoá chứ?',
-                    onConfirm: () => {
-                      DialogConfirm.close();
-                      deleteMagazine({ magazineId: magazine.id })
-                        .then(() => {
-                          mutate()
-                            .catch(logger.log)
-                            .finally(() => {
-                              router.back();
-                            });
-                        })
-                        .catch(err => {
-                          toast({
-                            variant: 'destructive',
-                            description: parseErrorMessage(err),
-                          });
-                        });
-                    },
-                  });
-                }, 200);
-              } else {
-                setMagazine(magazine);
-                router.push(MAGAZINE_ROUTES.createMagazine);
+          <div className="w-full inline-flex justify-between mt-[24px] mb-[4px]">
+            <button
+              className="inline-flex items-center gap-2 hover-scale"
+              onClick={() =>
+                goToProfile(magazine.creator, {
+                  router,
+                  myId: profile?.id,
+                })
               }
-            }}
+            >
+              <Avatar src={magazine.creator_avatar} size={35} />
+              <p>{magazine.creator_name}</p>
+            </button>
+            {isMine && (
+              <DropDown
+                trigger={
+                  <EllipsisVertical strokeWidth={1.4} size={20} className="hover-slow rounded-md" />
+                }
+                options={[
+                  {
+                    value: 'edit',
+                    label: 'Chỉnh sửa',
+                    type: 'menu-item',
+                  },
+                  {
+                    value: 'delete',
+                    label: 'Xoá bài',
+                    type: 'menu-item',
+                  },
+                ]}
+                onCheck={v => {
+                  if (v === 'delete') {
+                    setTimeout(() => {
+                      DialogConfirm.open({
+                        title: 'Bạn chắc chắn muốn xoá chứ?',
+                        onConfirm: () => {
+                          DialogConfirm.close();
+                          deleteMagazine({ magazineId: magazine.id })
+                            .then(() => {
+                              mutate()
+                                .catch(logger.log)
+                                .finally(() => {
+                                  router.back();
+                                });
+                            })
+                            .catch(err => {
+                              toast({
+                                variant: 'destructive',
+                                description: parseErrorMessage(err),
+                              });
+                            });
+                        },
+                      });
+                    }, 200);
+                  } else {
+                    setMagazine(magazine);
+                    router.push(MAGAZINE_ROUTES.createMagazine);
+                  }
+                }}
+              />
+            )}
+          </div>
+
+          {!!magazine.description && (
+            <p className="mt-[20px] mb-[4px] font-medium">{magazine.description}</p>
+          )}
+
+          <TableOfContent
+            listTitles={listTitles}
+            className="w-full inline-flex lg:hidden mt-[20px]"
           />
-        )}
+
+          {magazine.content.map((c, i) => {
+            return <Fragment key={i}>{renderContent(c)}</Fragment>;
+          })}
+        </div>
+
+        <TableOfContent
+          listTitles={listTitles}
+          className="w-[270px] hidden lg:inline-flex sticky top-8 max-h-[60vh] self-start"
+        />
       </div>
-
-      {!!magazine.description && (
-        <p className="mt-[20px] mb-[4px] font-medium">{magazine.description}</p>
-      )}
-
-      {magazine.content.map((c, i) => {
-        return <Fragment key={i}>{renderContent(c)}</Fragment>;
-      })}
     </Container>
   );
 };
